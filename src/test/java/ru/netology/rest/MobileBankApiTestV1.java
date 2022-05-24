24bfb4368a3bc773857c2545e4c4de35cd0d6656
Alina1103master <honor10alina@gmail.ru>
2022-05-24 13:49:33 +0300

    initial commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -7,8 +7,6 @@ import static io.restassured.RestAssured.given;
 public class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-        // Given - When - Then
-        // Предусловия
         given()
                 .baseUri("http://localhost:9999/api/v1")
                 // Выполняемые действия
